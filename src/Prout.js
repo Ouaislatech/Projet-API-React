235efa6d385b9a723852f45a2fa182e21482a157
Mélissa OUMAZA <oumazamelissa@MacBook-Pro-de-Mel.local>
2024-03-12 17:01:54 +0100

    Import React

---
+++ src/Prout.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+class Prout extends Component {
+    render () {
+        return (
+            <div className="Prout">
+                <h1>Maison</h1>
+            </div>
+        );
+    }
+}
+
+export default Prout;
